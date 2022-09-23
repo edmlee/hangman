773b62ad139f330f725f2bf667d8571e96854dfb
Eddie Lee <112559345+edmlee@users.noreply.github.com>
2022-09-23 23:29:53 +1000

    Update

--- hangman.py
+++ hangman.py
@@ -10,16 +10,17 @@ class Hangman():
 
         self.window = ck.CTk()
         self.window.title("Hangman")
-        self.window.geometry(f"{WIDTH}x{HEIGHT}")
-        self.window.minsize(WIDTH, HEIGHT)
+        self.window.geometry(f"{MAIN_WIDTH}x{MAIN_HEIGHT}")
+        self.window.minsize(MAIN_WIDTH, MAIN_HEIGHT)
         self.window.rowconfigure(0, weight=1)
         self.window.columnconfigure(0, weight=1)
 
         self.word_list = []
+        self.keyboard_buttons = {}
         self.word_variable = ck.StringVar()
         self.used_letters = set()
         self.status = STATUS[0]
-        self.keyboard_buttons = {}
+        self.difficulty = "Normal"
 
         self.load_word_list()
         self.create_frames()
@@ -52,37 +53,62 @@ class Hangman():
         self.side_frame.grid(row=0, column=1, rowspan=2, sticky="nesw")
 
 
+    def create_difficulty_window(self):
+        self.difficulty_window = ck.CTkToplevel()
+        self.difficulty_window.geometry(f"{POPUP_WIDTH}x{POPUP_HEIGHT}")
+
+        label = ck.CTkLabel(self.difficulty_window, text="Select Difficulty: ", text_font=KEYBOARD_FONT)
+        label.grid(padx=10, pady=20, sticky="w")
+        self.create_radio_buttons()
+        
+
     def create_button(self, frame, text, row, column, font, command):
         button = ck.CTkButton(frame, text=text, border_width=BORDER_WIDTH,
                               corner_radius=BUTTON_CORNER_RADIUS, text_font=font, command=command)
-        button.grid(row=row, column=column, sticky="nesw")
+        button.grid(row=row, column=column)
         return button
 
 
     def create_keyboard_buttons(self):
         row = 0
         column = 0
-        i = 0
         for letter in ALPHABET:
             if column == len(ALPHABET) / 2:
                 row += 1
                 column = 0
-            self.keyboard_buttons[i] = self.create_button(self.keyboard_frame, text=letter, row=row, column=column, font=KEYBOARD_FONT, 
-                                                          command= lambda letter=letter: self.update_word(letter))
-            self.keyboard_buttons[i].configure(height=BUTTON_HEIGHT)
+            self.keyboard_buttons[letter] = self.create_button(self.keyboard_frame, text=letter, 
+                                                               row=row, column=column, font=KEYBOARD_FONT, 
+                                                               command= lambda letter=letter: self.update_word(letter))
+            self.keyboard_buttons[letter].configure(height=BUTTON_HEIGHT)
             self.keyboard_frame.columnconfigure(column, weight=1)
             column += 1
-            i += 1
-
+ 
 
     def create_side_buttons(self):
-        self.new_game_button = self.create_button(self.side_frame, text="New Game", row=0, column=0, 
-                                             font=KEYBOARD_FONT, command=self.play_game)
+        self.new_game_button = self.create_button(self.side_frame, text="New Game", row=0, column=0,
+                                                  font=KEYBOARD_FONT, command=self.play_game)                                       
         self.new_game_button.configure(fg_color=GREEN, text_color=INNER_TEXT_COLOR, hover_color=GREEN_HOVER_COLOR)
-        
+        self.new_game_button.grid_configure(pady=(10, 0))  
+
+        self.difficulty_button = self.create_button(self.side_frame, text="Change\nDifficulty", row=4, column=0,
+                                                    font=KEYBOARD_FONT, command=self.create_difficulty_window)
+        self.difficulty_button.configure(fg_color=DIFFICULTY_COLOR, text_color=INNER_TEXT_COLOR, hover_color=DIFFICULTY_COLOR)
+
+        self.difficulty_button.grid_configure(pady=(0, 15))
+
         self.quit_button = self.create_button(self.side_frame, text="Quit", row=5, column=0, 
-                                         font=KEYBOARD_FONT, command=self.window.quit)
+                                              font=KEYBOARD_FONT, command=self.window.quit)
         self.quit_button.configure(fg_color=RED, text_color=INNER_TEXT_COLOR, hover_color=RED_HOVER_COLOR)
+
+
+    def create_radio_buttons(self):
+        var = ck.IntVar()
+        value = 1
+        for difficulty in DIFFICULTY.keys():
+            button = ck.CTkRadioButton(self.difficulty_window, text=difficulty, variable=var, value=value, text_font=DIFFICULTY_FONT)
+            button.grid(padx=20, sticky="w")
+            value += 1
+        return
 
 
     def create_labels(self):
@@ -92,20 +118,21 @@ class Hangman():
         self.word_label.grid(row=0, column=0)
         self.word_label.place(relx=0.5, rely=0.5, anchor=ck.CENTER)
 
-        self.lives_text_label = ck.CTkLabel(self.side_frame, width=1, text=f"Lives: {self.lives}", text_font=KEYBOARD_FONT)
-        self.lives_text_label.grid(row=1, column=0, pady=20)
+        self.lives_text_label = ck.CTkLabel(self.side_frame, text=f"Lives: {self.lives}", text_font=KEYBOARD_FONT)
+        self.lives_text_label.grid(row=1, column=0, pady=30)
 
+        self.difficulty_label = ck.CTkLabel(self.side_frame, text=f"Difficulty: {self.difficulty}", text_font=KEYBOARD_FONT)
+        self.difficulty_label.grid(row=3, column=0, padx=10, pady=(0, 20))
+        
     
-    def new_game(self):
+    def play_game(self):
         self.status = STATUS[0]
+        self.used_letters = set()
         self.lives = NUMBER_OF_LIVES
         self.lives_text_label.configure(text=f"Lives: {self.lives}")
-        for keys in self.keyboard_buttons:
-            self.keyboard_buttons[keys].configure(state=ck.NORMAL)
+        for letter in self.keyboard_buttons:
+            self.keyboard_buttons[letter].configure(fg_color=DARK_BLUE, state=ck.NORMAL)
 
-
-    def play_game(self):
-        self.new_game()        
         self.secret_word = random.choice(self.word_list)
         word = "_ " * len(self.secret_word)
         self.word_variable.set(word.strip())
@@ -127,16 +154,22 @@ class Hangman():
             if letter not in self.secret_word:
                 self.lives -= 1
             self.lives_text_label.configure(text=f"Lives: {self.lives}")
-            print(self.used_letters)
+            self.keyboard_buttons[letter].configure(fg_color=DISABLED_BUTTON_COLOR, state=ck.DISABLED)
+        self.update_status(word)
+
+        
+    def update_status(self, word):
         if self.lives == 0:
             self.status = STATUS[1]
-            print(self.status)
         elif set(word).issubset(self.used_letters):
             self.status = STATUS[2]
-            print(self.status)
-        if self.status == STATUS[1] or self.status == STATUS[2]:
-            for keys in self.keyboard_buttons:
-                self.keyboard_buttons[keys].configure(state=ck.DISABLED)
+        if self.status != STATUS[0]:
+            for letter in self.keyboard_buttons:
+                self.keyboard_buttons[letter].configure(state=ck.DISABLED)
+
+
+    def set_difficulty(self, min_length, max_length):           
+        pass
 
 
     def select_difficulty(self):
--- settings.py
+++ settings.py
@@ -3,28 +3,33 @@ BACKGROUND_COLOR = "#1F1F23"
 SIDE_BAR_COLOR = "#3C3C44"
 INNER_TEXT_COLOR = "black"
 DARK_BLUE = "#1F6AA5"
-GREEN = "green"
-GREEN_HOVER_COLOR = "#003300"
+GREEN = "#33CC33"
+GREEN_HOVER_COLOR = "#145214"
 RED = "red"
-RED_HOVER_COLOR = "yellow"
-KEYBOARD_FONT = ("Arial", 14)
-WORD_FONT = ("Arial", 24)
+RED_HOVER_COLOR = "#800000"
+DIFFICULTY_COLOR = "orange"
+DISABLED_BUTTON_COLOR = "#FFFF33"
+KEYBOARD_FONT = ("Helvetica", 14)
+DIFFICULTY_FONT = ("Helvetica", 12)
+WORD_FONT = ("Helvetica", 24)
 
 # Window, frame and button sizes
-WIDTH = 800
-HEIGHT = 300
+MAIN_WIDTH = 800
+MAIN_HEIGHT = 300
+POPUP_WIDTH = 200
+POPUP_HEIGHT = 200
 BUTTON_CORNER_RADIUS = 10
 BUTTON_HEIGHT = 60
 BORDER_WIDTH = 3
 
 # Word properties
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-STATUS = ("PLAYING", "LOST", "WON")
+STATUS = ("Playing", "Lose", "Win")
+DIFFICULTY = {
+              "Easy": (3, 5),
+              "Normal": (6, 8),
+              "Hard": (9, 100),
+              "Custom": (3, 100)
+              }
 MIN_WORD_LENGTH = 9
 NUMBER_OF_LIVES = 8
-EASY_MIN_LENGTH = 3
-EASY_MAX_LENGTH = 5
-MEDIUM_MIN_LENGTH = 6
-MEDIUM_MAX_LENGTH = 8
-HARD_MIN_LENGTH = 9
-ALL_MIN_LENGTH = 3
